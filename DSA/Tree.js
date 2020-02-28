dafc3881b3c3fec870726ebb09f2a2b98aa6d32d
xpectro93 <jonathanandrade@pursuit.org>
2020-02-27 22:42:45 -0500

    add to LL file

--- DSA/Tree.js
+++ DSA/Tree.js
@@ -1,7 +1,7 @@
 class TreeNode {
   constructor(val){
     this.val = val;
-    this.kiddos = []
+    this.descendants = [];
   }
 }
 class Tree{
@@ -13,6 +13,10 @@ let newTree = new Tree();
 let node1 = new TreeNode(2);
 let kid1 = new TreeNode(1);
 let kid2 = new TreeNode(3);
-node1.kiddos.push(kid1).push(kid2);
+node1.descendants.push(kid1);
+node1.descendants.push(kid2);
 
-console.log(node1)
+// Pre-Order Traversal => Node -> left -> right 
+// In-Order Traversal => left -> Node -> right 
+// Post-Order Traversal => left -> right -> Node
+// Reverse In-Order => right -> Node -> left
